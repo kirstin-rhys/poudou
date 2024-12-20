0e3721e281b9cbaaa243a9d343ac2b79a92b1621
kirstin penelope rhys <kirstin.rhys@gmail.com>
2024-12-19 19:46:26 -0800

    turn off eval and compile for general

--- lisp/poudou/keybindings/keybindings.el
+++ lisp/poudou/keybindings/keybindings.el
@@ -9,10 +9,10 @@
 
 (use-package general
   :demand t
-  :straight t
-  :init
-  (eval-and-compile
-    (require 'general)))
+  :straight t)
+  ;; :init
+  ;; (eval-and-compile
+  ;;   (require 'general)))
 
 
 (provide 'poudou-keybindings)
